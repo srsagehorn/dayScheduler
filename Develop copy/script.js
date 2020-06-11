5e4c2b4394e3c815b4b4e48cde722ee298575e1f
Shannon Sagehorn <srsagehorn@gmail.com>
2020-06-11 18:23:52 -0500

    Set up layout

---
+++ Develop copy/script.js
@@ -0,0 +1,18 @@
+// add time blocks
+// use api to color coat
+//     if time > grey
+// if time < red
+// if time = green
+if ($(".hour") == moment().hour(Number)) {
+  console.log(moment().hour);
+}
+// create a text input
+// create a save button
+//     prevent default
+// save input to html
+// save to local storage
+
+$("#dateTime").text(moment().format("LLLL"));
+
+// $("description").val();
+// localStorage.setItem(("description": description));
